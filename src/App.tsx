ce2a4868dde56b48c590492703f44fd5847f7f17
agusb19 <agustinbrage19@gmail.com>
2023-12-18 18:42:10 -0300

    feat: render components inside router provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import { BlogCards } from './components/BlogCards'
+import { BlogSections } from './components/BlogSections'
+import { GlobalProvider } from './context/GlobalProvider'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './App.css'
 
@@ -6,9 +9,22 @@ function App() {
 
   const queryClient = new QueryClient()
 
+  const router = createBrowserRouter([
+    {
+      path: '/blog-posts',
+      element: <BlogCards/>
+    },
+    {
+      path: '/blog-posts/:article',
+      element: <BlogSections/>
+    }
+  ])
+
   return (
     <QueryClientProvider client={queryClient}>
-      <BlogCards api_key='09f5621d01bad9cf9c10b8b2d6e209fc' />
+      <GlobalProvider api_key='09f5621d01bad9cf9c10b8b2d6e209fc' >
+        <RouterProvider router={router}/>
+      </GlobalProvider>
     </QueryClientProvider>
   )
 }
